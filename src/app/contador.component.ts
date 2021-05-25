9bdc790f7f873590ea04367305bc9123a537e2e7
marta.sanch.ger <marta.sanch.ger@gmail.com>
2021-05-25 10:13:43 +0200

    Siguiendo curso de Ángular

---
+++ src/app/contador.component.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'app-contador',
+    template: `
+    
+    <h1>{{title}}</h1>
+
+
+    <button (click)=" acumular(base) ">{{ base }}</button>
+    <span>{{ number }}</span>
+    
+    <button (click)=" acumular(-base) ">- {{ base }}</button>
+    `
+   
+})
+
+export class ContadorComponent {
+
+    public title : string = 'Contador App';
+    public number: number = 100 ; 
+    public base  : number = 5 ;
+  
+    acumular = (valor: number) => this.number += valor ;
+
+
+}
